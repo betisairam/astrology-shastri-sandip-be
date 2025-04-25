6c2d4d674458eb680a3c060db96968a06a8636bb
Sairam Beti <sairambeti@gmail.com>
2025-04-25 21:41:52 +0530

    updated knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,25 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || '127.0.0.1',
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'true'
     },
     migrations: {
       directory: './migrations'
